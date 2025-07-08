d8f12cbaa6632df5ba3b6a13029e27137210858e
chaicurioquest <ramavenkates@gmail.com>
2025-07-09 02:26:37 +0530

    working code

---
+++ org/roam-tags.el
@@ -0,0 +1,4 @@
+(defvar my-roam-tags
+  '("academics" "research" "personal" "notes" "projects" "students" "presentations" "events" "grants" "labwork" "concepts" "ideas" "references" "reviews" "draft" "final" "confidential" "internal" "publics")
+  "List of tags for org-roam Zettelkasten notes, filtered from .filetags.")
+(provide 'roam-tags)
