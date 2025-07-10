d9b6fdcc060c514d532ff5e45fa0467b3a018b39
chaicurioquest <ramavenkates@gmail.com>
2025-07-10 10:43:45 +0530

    updates files except org-roam package

--- org/roam-tags.el
+++ org/roam-tags.el
@@ -1,4 +1,4 @@
 (defvar my-roam-tags
-  '("academics" "research" "personal" "notes" "projects" "students" "presentations" "events" "grants" "labwork" "concepts" "ideas" "references" "reviews" "draft" "final" "confidential" "internal" "publics")
+  '("academics" "research" "personal" "notes" "projects" "students" "presentations" "events" "grants" "labwork" "concepts" "ideas" "references" "reviews" "draft" "final" "confidential" "internal" "public")
   "List of tags for org-roam Zettelkasten notes, filtered from .filetags.")
 (provide 'roam-tags)
